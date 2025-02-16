e794589d850e1b9d3bc380d0324ee5e2661dfa2f
arduriki <jordi.ardura.professional@gmail.com>
2025-02-16 10:31:48 +0100

    2.8 HTML templateing and inheritance

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from Snippetbox"))
+
+	// Base template is the *first* file
+	files := []string{
+		"ui/html/base.html",
+		"ui/html/partials/nav.html",
+		"ui/html/pages/home.html",
+	}
+
+	// Template set
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Write the content of the "base" template as the response body.
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
